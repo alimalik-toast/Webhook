db22cedeb4def7c04d76fc7f2d6370f4c6e48789
asm-delphi <91637857+asm-delphi@users.noreply.github.com>
2022-08-22 14:47:27 -0700

    Test

---
+++ flaskProject/app.py
@@ -0,0 +1,20 @@
+import subprocess
+from flask import Flask, request, make_response
+
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def respond():
+    data = request.json
+    status = data["Status"]
+    if status == "Verify":
+        subprocess.call("./verify.sh")
+    else:
+        subprocess.call("./ocs.sh")
+
+    return make_response("Hello World 2")
+
+
+if __name__ == '__main__':
+    app.run()
