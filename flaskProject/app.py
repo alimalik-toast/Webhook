e8ab2e59336f434f3396cde16bec519d03ca2de4
asm-delphi <91637857+asm-delphi@users.noreply.github.com>
2022-08-24 01:44:10 -0700

    Test

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -8,13 +8,17 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def respond():
     data = request.json
-    status = data["Status"]
-    if status == "Verify":
-        subprocess.call("./verify.sh")
-    else:
-        subprocess.call("./ocs.sh")
+    status = data["state"]
 
-    return make_response("Hello World 2")
+    if status == "AI":
+        subprocess.call("./verify.sh")
+        return make_response("Success")
+
+    elif status == "POS":
+        subprocess.call("./ocs.sh")
+        return make_response("Success")
+    else:
+        make_response("Failure")
 
 
 if __name__ == '__main__':
